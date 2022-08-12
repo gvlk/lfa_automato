84528e45b71ae83d1b88f5d629612fb4ff09ddcb
Gui <88752991+gvlk@users.noreply.github.com>
2022-08-12 02:26:31 -0300

    comparar_l + verificar_cadeia + reconhecer(totalmente funcionando com AFN e lambda-transição) + novos exemplos de autômato

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ class Automato:
 		self.estados = set()
 		self.alfabeto = alfabeto
 		self.linguagem = set()
+		self.verificar = True
 
 	class Nodo:
 
@@ -26,25 +27,38 @@ class Automato:
 			nodo = self.Nodo(estado)
 			self.estados.add(nodo)
 
-		if transicoes is not None:
-			self.criar_ponteiros(nodo, transicoes)
-		else:
-			return nodo
-
 		if self.inicial is None:
 			self.inicial = nodo
 
 		if final:
 			self.final.add(nodo)
 
+		if transicoes is not None:
+			self.criar_ponteiros(nodo, transicoes)
+		else:
+			return nodo
+
 	def criar_ponteiros(self, nodo, transicoes: dict):
-		for simbolo, estado in transicoes.items():
-			if estado == nodo.nome:
-				nodo_apontado = nodo
+		if 'lambda' in transicoes or 'λ' in transicoes:
+			self.alfabeto.add('λ')
+			if 'lambda' in transicoes:
+				transicoes['λ'] = transicoes.pop('lambda')
+		for simbolo, estados in transicoes.items():
+			if simbolo in self.alfabeto:
+				if type(estados) is not set:
+					estados = {estados}
+				for estado in estados:
+					if estado == nodo.nome:
+						nodo_apontado = nodo
+					else:
+						nodo_apontado = self.inserir(estado)
+					ponteiro = {simbolo: {nodo_apontado}}
+					if simbolo in nodo.ponteiros:
+						nodo.ponteiros[simbolo].add(nodo_apontado)
+					else:
+						nodo.ponteiros.update(ponteiro)
 			else:
-				nodo_apontado = self.inserir(estado)
-			ponteiro = {simbolo: {nodo_apontado}}
-			nodo.ponteiros.update(ponteiro)
+				raise ValueError(f"'{simbolo}' não está no dicionário do autômato '{self.nome}'")
 
 	def procurar_estado(self, estado):
 		for nodo in self.estados:
@@ -53,12 +67,32 @@ class Automato:
 		else:
 			return None
 
-	def reconhecer(self, cadeia: str = 'λ'):
-		if self.final:
-			aux = self.inicial
-			for simbolo in cadeia:
-				conjunto_estados = aux.ponteiros[simbolo].copy()
-				aux = conjunto_estados.pop()
+	def reconhecer(self, cadeia: str, aux=None):
+
+		if self.final:  # Verificar se o autômato possui estado final
+			if aux is None:
+				aux = self.inicial
+			if 'λ' in aux.ponteiros:  # Se o estado atual possui uma lambda-transição
+				conjunto_estados = aux.ponteiros['λ'].copy()
+				while conjunto_estados:
+					estado_alternativa = conjunto_estados.pop()  # Seleciona um estado aleatorio do conjunto
+					if self.reconhecer(cadeia, estado_alternativa):
+						return True
+
+			for i, simbolo in enumerate(cadeia):
+				try:
+					conjunto_estados = aux.ponteiros[simbolo].copy()
+					aux = conjunto_estados.pop()
+					subcadeia = cadeia[i+1:]
+					if 'λ' in aux.ponteiros:  # Se o novo estado possui uma lambda-transição
+						if self.reconhecer(subcadeia, aux):
+							return True
+					while conjunto_estados:  # Loop até que não existam mais estados no conjunto
+						estado_alternativa = conjunto_estados.pop()
+						if self.reconhecer(subcadeia, estado_alternativa):
+							return True
+				except KeyError:
+					return False
 			else:
 				if aux in self.final:
 					return True
@@ -67,14 +101,28 @@ class Automato:
 		else:
 			raise Exception("Não há estado final.")
 
-	def definir_linguagem(self, tam_max=6):
+	def verificar_cadeia(self, cadeia):
+		if all(ca in self.alfabeto for ca in cadeia):
+			self.verificar = False
+			return True
+		else:
+			return False
+
+	def definir_linguagem(self, tam_max):
+		self.verificar = False
 		for i in range(1, tam_max+1):
-			for sequencia in product(self.alfabeto, repeat=i):
+			for sequencia in product(self.alfabeto.difference({'λ'}), repeat=i):
 				cadeia = ''.join(sequencia)
 				if self.reconhecer(cadeia):
 					self.linguagem.add(cadeia)
 
-	def mostrar_linguagem(self):
-		self.definir_linguagem()
+	def mostrar_linguagem(self, tam_max=5):
+		self.definir_linguagem(tam_max)
 		lista = sorted(self.linguagem)
 		print(sorted(lista, key=len))
+
+	def comparar_l(self, comparado):
+		if self.linguagem == comparado.linguagem:
+			return True
+		else:
+			return False
